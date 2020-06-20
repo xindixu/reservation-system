ab21264bfb1ad4199f8d560552142a2e4c050a18
Xindi Xu <xindixu@utexas.edu>
2020-06-20 03:08:11 -0500

    model/client

---
+++ db/migrate/20200620080724_create_clients.rb
@@ -0,0 +1,14 @@
+class CreateClients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.belongs_to :manager, null: false, foreign_key: true
+      t.integer :cycle
+
+      t.timestamps
+    end
+  end
+end
