126f0976704662e9ffe062eb518633363f488fea
Xindi Xu <xindixu@utexas.edu>
2020-06-20 03:07:46 -0500

    graphql/update treatment, still erroring

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -17,5 +17,34 @@ module Types
 
 
     # /treatments
+    field :treatments, [Types::TreatmentType], null: false
+
+    def treatments
+      Treatment.all
+    end
+
+    field :treatment, Types::TreatmentType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def treatment(id:)
+      Treatment.find(id)
+    end
+
+    # /teams
+    field :teams, [Types::TeamType], null: false
+
+    def teams
+      Team.all
+    end
+
+    field :team, Types::TeamType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def team(id:)
+      Team.find(id)
+    end
+    
   end
 end
--- app/graphql/types/treatment_type.rb
+++ app/graphql/types/treatment_type.rb
@@ -1,5 +1,6 @@
 module Types
   class TreatmentType < Types::BaseObject
+    field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: false
   end
