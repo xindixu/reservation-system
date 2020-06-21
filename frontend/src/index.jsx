91d3849e782bf739ccec297b7c6cf93d5b5fd640
Xindi Xu <xindixu@utexas.edu>
2020-06-20 21:44:56 -0500

    frontend/clean up react, set up eslint, postcss

---
+++ frontend/src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
