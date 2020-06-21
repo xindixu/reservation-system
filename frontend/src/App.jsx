ae382fd08acf891bbc51f7a5e7bd737a717a8b63
Xindi Xu <xindixu@utexas.edu>
2020-06-21 13:00:11 -0500

    frontend/add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Teams from './pages/teams';
+import Calendar from './pages/calendar';
 
 function App() {
   return (
-    <Teams />
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Calendar />
+        </Route>
+        <Route path="/teams" exact>
+          <Teams />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
