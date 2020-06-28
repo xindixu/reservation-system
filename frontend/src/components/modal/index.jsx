e1af37db74d215b65d45ee955b4c00e78cd1ab19
Xindi Xu <xindixu@utexas.edu>
2020-06-28 18:40:12 -0500

    Modal

--- frontend/src/components/modal/index.jsx
+++ frontend/src/components/modal/index.jsx
@@ -1,57 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 
+import {
+  Button,
+  Modal as BSModal,
+} from 'reactstrap';
 
 const Modal = ({
-  show, title, children, footer, onClose,
-}) => {
-  const [containerEl] = useState(document.getElementById('modal-root'));
-
-  return ReactDOM.createPortal(show ? (
-    <>
-      <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+  show, onClose, title, children, submitButton,
+}) => (
+  <BSModal
+    className="modal-dialog-centered"
+    isOpen={show}
+    toggle={onClose}
+  >
+    <div className="modal-header">
+      <h5 className="modal-title">
+        {title}
+      </h5>
+      <button
+        aria-label="Close"
+        className="close"
+        data-dismiss="modal"
+        type="button"
+        onClick={onClose}
       >
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          {/* content */}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            {/* header */}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-              <h1 className="text-3xl font-semibold">
-                {title}
-              </h1>
-
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                type="button"
-                onClick={onClose}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
-            </div>
-            {/* body */}
-            <div className="relative p-6 flex-auto">
-              <p className="my-4 text-gray-600 text-lg leading-relaxed">
-                {children}
-              </p>
-            </div>
-            {/* footer */}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
-              {footer}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black" />
-    </>
-  ) : null, containerEl);
-};
+        <span aria-hidden>×</span>
+      </button>
+    </div>
+    <div className="modal-body">{children}</div>
+    <div className="modal-footer">
+      <Button
+        color="secondary"
+        data-dismiss="modal"
+        type="button"
+        onClick={onClose}
+      >
+        Close
+      </Button>
+      {submitButton}
+    </div>
+  </BSModal>
+);
 
 Modal.propTypes = {
-
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  submitButton: PropTypes.node.isRequired,
 };
 
 export default Modal;
