caadc1bc396a3c3471812d31924f239d644f98d7
Xindi Xu <xindixu@utexas.edu>
2020-07-05 20:39:18 -0500

    Update navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
+import { useMedia } from "react-use"
+import { debounce } from "lodash"
 import { Layout } from "antd"
 import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons"
 import Routes from "./routes"
 import Navbar from "./components/navbar"
 import { Wrapper } from "./styles"
+import { mediaQuery } from "./styles/index"
 
 const { Header, Sider, Content } = Layout
 
 const App = () => {
   const [navigationCollapsed, setNavigationCollapsed] = useState(false)
+  const [navigationToggled, setNavigationToggled] = useState(false)
+  const smAndUp = useMedia(mediaQuery.screenSmAndUp)
+  const mdAndUp = useMedia(mediaQuery.screenMdAndUp)
+
+  useEffect(() => {
+    const resetToggled = debounce(() => setNavigationToggled(false), 100)
+    window.addEventListener("resize", resetToggled)
+    return () => {
+      window.removeEventListener("resize", resetToggled)
+    }
+  }, [])
+
+  const collapsed = navigationToggled ? navigationCollapsed : !mdAndUp
+
   return (
     <Wrapper>
       <Router>
         <Layout>
-          <Sider trigger={null} collapsible collapsed={navigationCollapsed}>
+          <Sider trigger={null} collapsed={collapsed} collapsedWidth={smAndUp ? "80" : "0"}>
             <Navbar />
           </Sider>
           <Layout>
@@ -22,12 +39,18 @@ const App = () => {
               {navigationCollapsed ? (
                 <MenuUnfoldOutlined
                   className="trigger"
-                  onClick={() => setNavigationCollapsed(false)}
+                  onClick={() => {
+                    setNavigationCollapsed(false)
+                    setNavigationToggled(true)
+                  }}
                 />
               ) : (
                 <MenuFoldOutlined
                   className="trigger"
-                  onClick={() => setNavigationCollapsed(true)}
+                  onClick={() => {
+                    setNavigationCollapsed(true)
+                    setNavigationToggled(true)
+                  }}
                 />
               )}
             </Header>
