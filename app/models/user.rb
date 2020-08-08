7c9da9c55fa172ac74689d50227253cb54dd9724
Xindi Xu <xindixu@utexas.edu>
2020-08-08 17:52:10 -0500

    create devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,37 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_secure_password
+
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  include Tokenizable
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable, 
+         :devise,
+         :validatable,
+         :trackable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: self
 
   validates :email, presence: true, uniqueness: true
   enum role: %i[client manager admin]
 
-  after_initialize do
-    self.role ||= :manager if new_record?
+  after_initialize :setup_new_user, if: :new_record?
+
+  # Send mail through activejob
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  private
+  
+  def setup_new_user
+    self.role ||= :manager
   end
 end
