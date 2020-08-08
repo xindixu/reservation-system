893cc6a7d13cda82b295a9ded1a0609a749bc4bc
Xindi Xu <xindixu@utexas.edu>
2020-08-08 01:00:36 -0500

    create toolbar for filtering

--- frontend/src/pages/calendar/index.jsx
+++ frontend/src/pages/calendar/index.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from "react"
-import { useQuery, useMutation } from "@apollo/react-hooks"
-
-import Toolbar from "./toolbar"
-import { GET_ALL_VISITS, CREATE_VISIT, UPDATE_VISIT, DESTROY_VISIT } from "graphql/visits"
-import Calendar from "components/calendar"
-import Modal from "components/modal"
-import VisitForm from "components/forms/visit-form"
-import FAButton from "components/floating-action-button"
-import { getFullName } from "lib/utils"
-import { toISOStringWithTZ } from "lib/datetime"
-
-const MODALS = {
-  addVisit: "addVisit",
-  editVisit: "editVisit",
-}
+import { useQuery, useLazyQuery, useMutation } from "@apollo/react-hooks"
+import { isEmpty } from "lodash"
+import { Spin } from "antd"
+import Page from "./page"
+import {
+  GET_ALL_VISITS,
+  SEARCH_VISITS,
+  CREATE_VISIT,
+  UPDATE_VISIT,
+  DESTROY_VISIT,
+} from "graphql/visits"
 
 const CalendarPage = () => {
-  const { loading, error, data } = useQuery(GET_ALL_VISITS)
+  const [searchParams, setSearchParams] = useState({})
+  const { loading: loadingAllVisits, error, data: allVisitData } = useQuery(GET_ALL_VISITS)
+  const [searchVisits, { loading: loadingSearchVisits, data: searchedVisitsData }] = useLazyQuery(
+    SEARCH_VISITS,
+    {
+      variables: searchParams,
+    }
+  )
   const [addVisit] = useMutation(CREATE_VISIT, {
     update: (cache, { data: { createVisit } }) => {
       const { visit } = createVisit
@@ -44,85 +47,24 @@ const CalendarPage = () => {
     },
   })
 
-  const [selectedVisit, setSelectedVisit] = useState({})
-  const [presetDate, setPresetDate] = useState({})
-  const [modalToShow, setModalToShow] = useState("")
-  if (loading) {
-    return "loading..."
-  }
   if (error) {
     return `Error ${error.message}`
   }
 
-  const { visits } = data
-
-  const modalOnCloseAndReset = () => {
-    setModalToShow("")
-    setPresetDate({})
-    setSelectedVisit({})
-  }
-
+  const visits = isEmpty(searchParams) ? allVisitData?.visits : searchedVisitsData?.searchVisits
+  const loading = isEmpty(searchParams) ? loadingAllVisits : loadingSearchVisits
   return (
     <>
-      <Toolbar onFilterChange={(filter) => console.log(filter)} />
-      <Calendar
-        visits={visits}
-        onClickVisit={(id) => {
-          setSelectedVisit(visits.find((v) => v.id === id))
-          setModalToShow(MODALS.editVisit)
-        }}
-        onEditVisit={(id, start, end) => {
-          editVisit({
-            variables: {
-              id,
-              startsAt: toISOStringWithTZ(new Date(start)),
-              endsAt: toISOStringWithTZ(new Date(end)),
-            },
-          })
-        }}
-        onSelectDateRange={(startsAt, endsAt, allDay) => {
-          setPresetDate({ startsAt, endsAt, allDay })
-          setModalToShow(MODALS.addVisit)
-        }}
-      />
-      {modalToShow === MODALS.editVisit && (
-        <Modal
-          title={`Edit Visit for ${getFullName(selectedVisit.client)}`}
-          onClose={modalOnCloseAndReset}
-          onDelete={() => {
-            deleteVisit({ variables: { id: selectedVisit.id } })
-          }}
-          submitButtonText="Update"
-        >
-          {({ form }) => (
-            <VisitForm
-              initialVisit={selectedVisit}
-              form={form}
-              disabled={{ clientId: true }}
-              onSubmit={(values) => {
-                editVisit({ variables: { id: selectedVisit.id, ...values } })
-                setSelectedVisit({})
-              }}
-            />
-          )}
-        </Modal>
-      )}
-      {modalToShow === MODALS.addVisit && (
-        <Modal title="Create New Visit" onClose={modalOnCloseAndReset} submitButtonText="Create">
-          {({ form }) => (
-            <VisitForm
-              form={form}
-              onSubmit={(values) => addVisit({ variables: values })}
-              initialVisit={{ ...presetDate }}
-            />
-          )}
-        </Modal>
-      )}
-      <FAButton
-        onClick={() => setModalToShow(MODALS.addVisit)}
-        ariaLabel="New Visit"
-        rotate={!!modalToShow}
-      />
+      <Spin spinning={loading}>
+        <Page
+          visits={visits || []}
+          setSearchParams={setSearchParams}
+          searchVisits={searchVisits}
+          editVisit={editVisit}
+          deleteVisit={deleteVisit}
+          addVisit={addVisit}
+        />
+      </Spin>
     </>
   )
 }
--- frontend/src/pages/calendar/toolbar.jsx
+++ frontend/src/pages/calendar/toolbar.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useLazyQuery } from "@apollo/react-hooks"
-import { Form, Select, Row, Col, Button } from "antd"
+import { Form, Select, Row, Col, Button, Collapse, Spin } from "antd"
 import { GET_ALL_MANAGERS } from "graphql/managers"
 import { GET_ALL_CLIENTS } from "graphql/clients"
 import { getFullName } from "lib/utils"
 import { GET_ALL_SLOTS } from "graphql/slots"
 
 const { Option } = Select
+const { Panel } = Collapse
 
 const Toolbar = ({ onFilterChange }) => {
+  const [form] = Form.useForm()
+
   const [loadManagers, { loading: loadingManagers, data: managersData }] = useLazyQuery(
     GET_ALL_MANAGERS
   )
@@ -46,41 +49,61 @@ const Toolbar = ({ onFilterChange }) => {
   ]
 
   const onFinish = (fieldValues) => {
-    console.log(fieldValues)
     onFilterChange(fieldValues)
   }
 
   return (
-    <Form className="mb-4 w-full flex" onFinish={onFinish}>
-      <Row gutter={24} className="w-full">
-        {filters.map(({ label, name, onFocus, loading, options, itemToString }) => (
-          <Col span={8} key={name}>
-            <Form.Item label={label} name={name}>
-              <Select
-                allowClear
-                onFocus={onFocus}
-                mode="multiple"
-                placeholder="Select managers"
-                optionFilterProp="children"
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().includes(input.toLowerCase())
-                }
-                loading={loading}
-              >
-                {options &&
-                  options.map((opt) => <Option value={opt.id}>{itemToString(opt)}</Option>)}
-              </Select>
-            </Form.Item>
-          </Col>
-        ))}
-      </Row>
-      <Button type="primary" htmlType="submit">
-        Filter
-      </Button>
-    </Form>
+    <Collapse defaultActiveKey={["1"]} bordered={false} className="mb-4">
+      <Panel header="Filter by..." key="1">
+        <Form form={form} className="mb-4 flex-wrap" onFinish={onFinish} layout="vertical">
+          <Row gutter={24} className="w-full">
+            {filters.map(({ label, name, onFocus, loading, options, itemToString }) => (
+              <Col span={8} key={name}>
+                <Form.Item label={label} name={name}>
+                  <Spin spinning={loading}>
+                    <Select
+                      allowClear
+                      onFocus={onFocus}
+                      mode="multiple"
+                      placeholder={`Select one or multiple ${label}`}
+                      optionFilterProp="children"
+                      filterOption={(input, option) =>
+                        option.children.toLowerCase().includes(input.toLowerCase())
+                      }
+                      loading={loading}
+                    >
+                      {options &&
+                        options.map((opt) => <Option value={opt.id}>{itemToString(opt)}</Option>)}
+                    </Select>
+                  </Spin>
+                </Form.Item>
+              </Col>
+            ))}
+          </Row>
+
+          <Form.Item>
+            <Button
+              htmlType="reset"
+              onClick={() => {
+                form.resetFields()
+                form.submit()
+              }}
+              className="mr-2"
+            >
+              Clear
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Filter
+            </Button>
+          </Form.Item>
+        </Form>
+      </Panel>
+    </Collapse>
   )
 }
 
-Toolbar.propTypes = {}
+Toolbar.propTypes = {
+  onFilterChange: PropTypes.func.isRequired,
+}
 
 export default Toolbar
