a71499819b445b74519533881f368300c0a4665b
Xindi Xu <xindixu@utexas.edu>
2020-08-08 01:48:13 -0500

    bugfix/select can't be wrapped by spin, otherwise onChange will broke

--- frontend/src/pages/calendar/toolbar.jsx
+++ frontend/src/pages/calendar/toolbar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useLazyQuery } from "@apollo/react-hooks"
-import { Form, Select, Row, Col, Button, Collapse, Spin } from "antd"
+import { Form, Select, Row, Col, Button, Collapse } from "antd"
 import { GET_ALL_MANAGERS } from "graphql/managers"
 import { GET_ALL_CLIENTS } from "graphql/clients"
 import { getFullName } from "lib/utils"
@@ -51,50 +51,52 @@ const Toolbar = ({ onFilterChange }) => {
   const onFinish = (fieldValues) => {
     onFilterChange(fieldValues)
   }
-
   return (
-    <Collapse defaultActiveKey={["1"]} bordered={false} className="mb-4">
-      <Panel header="Filter by..." key="1">
+    <Collapse defaultActiveKey={["filter-comp"]} bordered={false} className="mb-4">
+      <Panel header="Filter by..." key="filter-comp">
         <Form form={form} className="mb-4 flex-wrap" onFinish={onFinish} layout="vertical">
           <Row gutter={24} className="w-full">
             {filters.map(({ label, name, onFocus, loading, options, itemToString }) => (
               <Col span={8} key={name}>
                 <Form.Item label={label} name={name}>
-                  <Spin spinning={loading}>
-                    <Select
-                      allowClear
-                      onFocus={onFocus}
-                      mode="multiple"
-                      placeholder={`Select one or multiple ${label}`}
-                      optionFilterProp="children"
-                      filterOption={(input, option) =>
-                        option.children.toLowerCase().includes(input.toLowerCase())
-                      }
-                      loading={loading}
-                    >
-                      {options &&
-                        options.map((opt) => <Option value={opt.id}>{itemToString(opt)}</Option>)}
-                    </Select>
-                  </Spin>
+                  <Select
+                    allowClear
+                    onFocus={onFocus}
+                    mode="multiple"
+                    placeholder={`Select one or multiple ${label}`}
+                    optionFilterProp="children"
+                    filterOption={(input, option) =>
+                      option.children.toLowerCase().includes(input.toLowerCase())
+                    }
+                    loading={loading}
+                  >
+                    {options &&
+                      options.map((opt) => (
+                        <Option key={`${name}-${opt.id}`} value={opt.id}>
+                          {itemToString(opt)}
+                        </Option>
+                      ))}
+                  </Select>
                 </Form.Item>
               </Col>
             ))}
           </Row>
 
           <Form.Item>
-            <Button
-              htmlType="reset"
-              onClick={() => {
-                form.resetFields()
-                form.submit()
-              }}
-              className="mr-2"
-            >
-              Clear
-            </Button>
-            <Button type="primary" htmlType="submit">
-              Filter
-            </Button>
+            <div className="flex justify-end space-x-2 pr-6">
+              <Button
+                htmlType="reset"
+                onClick={() => {
+                  form.resetFields()
+                  form.submit()
+                }}
+              >
+                Clear
+              </Button>
+              <Button type="primary" htmlType="submit" onClick={form.submit}>
+                Filter
+              </Button>
+            </div>
           </Form.Item>
         </Form>
       </Panel>
