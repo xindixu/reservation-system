4172da164b171ce2f72713380a891b87f9189c1d
Xindi Xu <xindixu@utexas.edu>
2020-08-08 22:15:05 -0500

    add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,35 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_secure_password
 
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-  include Tokenizable
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable, 
-         :devise,
-         :validatable,
-         :trackable,
-         :jwt_authenticatable,
-         jwt_revocation_strategy: self
-
-  validates :email, presence: true, uniqueness: true
   enum role: %i[client manager admin]
 
-  after_initialize :setup_new_user, if: :new_record?
-
-  # Send mail through activejob
-  def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
-  end
+  after_initialize :setup_user, if: :new_record?
 
   private
-  
-  def setup_new_user
-    self.role ||= :manager
+
+  def setup_user
+    self.role ||= :client
   end
 end
