31482ad1eaa747cca408d24e4b1edbd34b07492e
Xindi Xu <xindixu@utexas.edu>
2020-08-12 22:25:31 -0500

    set up basic apollo server

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 import express from "express"
 import bodyParser from "body-parser"
-import { graphqlHTTP } from "express-graphql"
+import { ApolloServer, gql } from "apollo-server-express"
 import mongoose from "mongoose"
-import graphqlSchema from "./graphql/schema/index.js"
-import graphqlResolvers from "./graphql/resolvers/index.js"
-import isAuth from "./middleware/is-auth.js"
+// import typeDefs from "./graphql/schema/index.js"
+// import resolvers from "./graphql/resolvers/index.js"
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => "yay",
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 const app = express()
-app.use(bodyParser.json())
-app.use(isAuth)
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: graphqlSchema,
-    rootValue: graphqlResolvers,
-    graphiql: true,
-  })
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@reservation-system.bqumh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true }
-  )
-  .then(() => app.listen(3000))
-  .catch((err) => console.error(err))
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@reservation-system.bqumh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+)
+
+mongoose.connection.once("open", () => console.log("🥭 MongoDB is connected!"))
