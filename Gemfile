14fe3fb36c9169e80c0360dd0982dd84246f4c32
Xindi Xu <xindixu@utexas.edu>
2020-08-12 20:51:48 -0500

    temp

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'image_processing', '~> 1.2'
 
 gem 'devise'
-gem 'devise-jwt', '~> 0.6.0'
+gem 'graphql_devise'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'jwt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,93 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_023124) do
+ActiveRecord::Schema.define(version: 2020_08_08_222230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "clients", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.integer "cycle"
-    t.integer "duration"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "managers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.string "job_title"
-    t.bigint "team_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["team_id"], name: "index_managers_on_team_id"
-  end
-
-  create_table "services", force: :cascade do |t|
-    t.bigint "manager_id", null: false
-    t.bigint "client_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["client_id"], name: "index_services_on_client_id"
-    t.index ["manager_id"], name: "index_services_on_manager_id"
-  end
-
-  create_table "slots", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.boolean "shareable"
-    t.bigint "manager_id", null: false
-    t.bigint "team_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["manager_id"], name: "index_slots_on_manager_id"
-    t.index ["team_id"], name: "index_slots_on_team_id"
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "email"
-    t.string "phone"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "visits", force: :cascade do |t|
-    t.datetime "starts_at"
-    t.datetime "ends_at"
-    t.bigint "client_id", null: false
-    t.bigint "slot_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["client_id"], name: "index_visits_on_client_id"
-    t.index ["slot_id"], name: "index_visits_on_slot_id"
-  end
-
-  add_foreign_key "managers", "teams"
-  add_foreign_key "services", "clients"
-  add_foreign_key "services", "managers"
-  add_foreign_key "slots", "managers"
-  add_foreign_key "slots", "teams"
-  add_foreign_key "visits", "clients"
-  add_foreign_key "visits", "slots"
 end
