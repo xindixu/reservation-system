5a87b459029d94d7fa9435b8e7cb7009cd0d0ee8
Xindi Xu <xindixu@utexas.edu>
2020-08-30 01:09:51 -0500

    sign up/style page

--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -1,29 +1,34 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, { useContext } from "react"
 import { Form } from "antd"
+import { useMedia } from "react-use"
+import { mediaQuery } from "styles/index"
+import { UserContext } from "contexts"
 import { ReactComponent as Events } from "assets/events.svg"
 import SignUpForm from "components/forms/sign-up-form"
 
 const SignUp = (props) => {
   const [form] = Form.useForm()
+  const xlAndUp = useMedia(mediaQuery.screenXlAndUp)
 
   return (
-    <div className="flex items-center justify-center bg-blue-300 w-screen h-screen">
-      <div className="flex p-10 bg-opacity-75 bg-white border-blue-200 rounded-lg border">
-        <div className="flex flex-col content-center mr-5 max-w-md">
+    <div className={`flex bg-blue-300 w-screen h-screen  ${xlAndUp ? "p-32" : "p-0"}`}>
+      <div className="flex justify-center content-center w-full h-full p-20 bg-opacity-75 bg-white border-blue-200 rounded-lg border">
+        <div className="flex flex-col justify-center content-center max-w-md">
           <h1 className="text-2xl">Welcome to Reservation System</h1>
           <p className="my-10">
             Reservation System can help you schedule recurrent client visits, check available slots,
             and remind you and your clients on upcoming visits.{" "}
           </p>
-          <SignUpForm form={form} />
+          <SignUpForm form={form} onSubmit={(values) => console.log(values)} />
         </div>
-        <Events />
+        {xlAndUp && (
+          <div className="ml-10 w-3/4">
+            <Events style={{ width: "100%" }} />
+          </div>
+        )}
       </div>
     </div>
   )
 }
-
-SignUp.propTypes = {}
 
 export default SignUp
