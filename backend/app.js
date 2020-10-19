05c3fc98dab357c429f2eedbbea461ddfbe0b800
Xindi Xu <xindixu@utexas.edu>
2020-10-19 00:37:07 -0500

    deploy/port setting

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,7 @@ app.use(async (req, res, next) => {
 
 server.applyMiddleware({ app, cors: corsOptions })
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
 
