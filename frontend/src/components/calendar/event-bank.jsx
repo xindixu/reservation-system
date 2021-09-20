c12707598d5a6818ce27242076b0127a821f56d7
Xindi Xu <xindixu0@gmail.com>
2021-09-19 20:19:37 -0400

    feat: update pkgs

--- frontend/src/components/calendar/event-bank.jsx
+++ frontend/src/components/calendar/event-bank.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const EventBank = ({ setDraggedEvent }) => {
-  return (
-    <span
-      draggable="true"
-      key="a"
-      onDragStart={() => setDraggedEvent({ title: "event-a", duration: 5 })}
-    >
-      Event A
-    </span>
-  )
-}
+const EventBank = ({ setDraggedEvent }) => (
+  <span
+    draggable="true"
+    key="a"
+    onDragStart={() => setDraggedEvent({ title: "event-a", duration: 5 })}
+  >
+    Event A
+  </span>
+)
 
-EventBank.propTypes = {}
+EventBank.propTypes = {
+  setDraggedEvent: PropTypes.func.isRequired,
+}
 
 export default EventBank
